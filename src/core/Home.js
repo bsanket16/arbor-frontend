777a2bd9c5303f54f36dbd22f1050eb1431d2797
sankt <bsankt16@gmail.com>
2020-07-19 19:57:08 +0530

    setting up env

--- src/core/Home.js
+++ src/core/Home.js
@@ -3,6 +3,9 @@ import React from 'react'
 import '../styles.css'
 
 export default function Home() {
+
+    console.log('API IS', process.env.REACT_APP_BACKEND)
+
     return (
         <div>
             <h1 className='text-white'>Hello Frontend</h1>
