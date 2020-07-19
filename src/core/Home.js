34b6abb34d34a020f0919d3a0d4af1b91d08b102
sankt <bsankt16@gmail.com>
2020-07-19 20:29:20 +0530

    connecting to database

--- src/core/Home.js
+++ src/core/Home.js
@@ -1,14 +1,15 @@
 import React from 'react'
 
+import { API } from '../backend'
 import '../styles.css'
 
 export default function Home() {
 
-    console.log('API IS', process.env.REACT_APP_BACKEND)
+    console.log('API IS', API)
 
     return (
         <div>
-            <h1 className='text-white'>Hello Frontend</h1>
+            <h1 className='text-white'>Hello Frontend { API }</h1>
         </div>
     )
 }
